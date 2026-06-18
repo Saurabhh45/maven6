1e107174dcff939cc0f5a70874fee5bd90e65c52
Saurabhh45 <saurabh.cse24le@cmrit.ac.in>
2026-06-18 05:13:25 +0000

    Initial commit

---
+++ src/main/java/hello45/App.java
@@ -0,0 +1,13 @@
+package hello45;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!1111111111" );
+    }
+}
